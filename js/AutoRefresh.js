b61bcb920e4b4f0e656faefbc5afeaa6d3d0d66d
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-08-24 13:32:41 -0700

    Auto-refresh no longer jumps you back to the inbox when you're looking at a folder.

--- js/AutoRefresh.js
+++ js/AutoRefresh.js
@@ -2,10 +2,15 @@ this.ymail = this.ymail || {};
 
 (function(module) {
 	
+	var $inboxLink;
+
 	module.AutoRefresh = function() {
+		$inboxLink = $('#storm-listnav a').first();
         setInterval(function() {
-            $('#btn-checkmail').click();
-        }, 5000)
+        	if ($inboxLink.attr('aria-selected') == 'true') {
+        		$('#btn-checkmail').click();	
+        	}
+        }, 5000);
     }
 
 })(this.ymail);
